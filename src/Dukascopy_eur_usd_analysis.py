d990b85754b63855ada46b828c963314652200bc
Serrat96 <serrat_torres@hotmail.com>
2021-02-28 12:33:34 +0100

    Antes de empezar, domingo

--- src/Dukascopy_eur_usd_analysis.py
+++ src/Dukascopy_eur_usd_analysis.py
@@ -6,20 +6,3 @@ eur_usd = dda.eur_usd
 
 print(eur_usd.head())
 
-eur_usd.rename(columns={'Gmt time': 'Date'}, inplace= True)
-
-print(eur_usd.head())
-
-eur_usd [['Date', 'Time']] = eur_usd.Date.str.split(expand=True)
-
-print(eur_usd.head())
-
-eur_usd = eur_usd[['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume']]
-
-print(eur_usd.head())
-
-eur_usd.Date = pd.to_datetime(eur_usd.Date)
-
-print(eur_usd.info())
-    
-    
