a2524ee66eb3598ccbb4f5f52106492719ae7932
Serrat96 <serrat_torres@hotmail.com>
2021-03-02 16:51:15 +0100

    importacion de datos subcarpeta

---
+++ Eur_dataframe_proccess.py
@@ -0,0 +1,10 @@
+import src.currencies_data.eur_list_1H as eur1h
+
+import src.currencies_data.eur_list_4H as eur4h
+
+import src.currencies_data.eur_list_1D as eur1d
+
+import src.currencies_data.eur_list_1W as eur1w
+
+import src.currencies_data.eur_list_1M as eur1m
+
